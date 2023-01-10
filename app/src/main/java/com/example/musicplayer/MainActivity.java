003b788ee45d129ab8ecbe6cfa215693b76a30d6
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-01-10 12:41:48 +0530

    Back Button Problem Fixed

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.musicplayer;
 
 import androidx.annotation.NonNull;
+
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -25,12 +32,33 @@ import com.karumi.dexter.listener.single.PermissionListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     //public static HashMap<String,String> mp;
-    public static ArrayList<File> songs;
+    public static ArrayList<File> Songs;
+
+    //Method that returns an ArrayList of Files whose name ends with .mp3 and does not start with a .
+    public static ArrayList<File> getSongs(File file){
+        ArrayList<File> ans = new ArrayList<>();
+        File[] listFiles = file.listFiles();
+        if(listFiles!=null){
+            for(File curr:listFiles){
+                if(!curr.isHidden() && curr.isDirectory()){
+                    ans.addAll(getSongs(curr));
+                }
+                else{
+                    if(curr.getName().endsWith(".mp3") && !curr.getName().startsWith(".")){
+                        ans.add(curr);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,18 +70,56 @@ public class MainActivity extends AppCompatActivity {
                 .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
                     //If Permission granted
+
+
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
 
+
+//                        StorageManager storageManager = (StorageManager) getSystemService(STORAGE_SERVICE);
+//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                            List<StorageVolume> volumes = storageManager.getStorageVolumes();
+//                            int i=0;
+//                            while(i<volumes.size()){
+//                                Log.d("MyLog",volumes.get(i).toString());
+//                                i++;
+//                            }
+//                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//                                if (volumes.get(0).getDirectory()==Environment.getExternalStorageDirectory()){
+//                                    Log.d("MyLog","0Internal");
+//                                }
+//                                else{
+//                                    Log.d("MyLog","0External");
+//                                }
+//                            }
+//                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//                                if (volumes.get(1).getDirectory()==Environment.getExternalStorageDirectory()){
+//                                    Log.d("MyLog","1Internal");
+//                                }
+//                                else{
+//                                    Log.d("MyLog","1External");
+//                                }
+//                            }
+//                        }
+
+                        //StorageManager storageManager = (StorageManager)getSystemService(STORAGE_SERVICE);
+                        //List<StorageVolume> vol = storageManager.getStorageVolumes();
+
+
+
                         //Getting a List of Songs in the form of a String
-                        songs = getSongs(Environment.getExternalStorageDirectory());
+                        //songs = getSongs(vol.get(1).getDirectory());
+                        Songs = getSongs(Environment.getExternalStorageDirectory());
 
 
                         //Setting Recycler View
-
-                        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-                        CustomAdapter adapter = new CustomAdapter(songs);
+                        LinearLayoutManager llm = new LinearLayoutManager(MainActivity.this);
+                        recyclerView.setLayoutManager(llm);
+                        CustomAdapter adapter = new CustomAdapter(Songs);
                         recyclerView.setAdapter(adapter);
+                        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                                llm.getOrientation());
+                        recyclerView.addItemDecoration(dividerItemDecoration);
                         //recyclerView.se
 
 
@@ -75,25 +141,31 @@ public class MainActivity extends AppCompatActivity {
                 .check();
     }
 
-    //Method that returns an ArrayList of Files whose name ends with .mp3 and does not start with a .
-    public static ArrayList<File> getSongs(File file){
-        ArrayList<File> ans = new ArrayList<>();
-        File[] listFiles = file.listFiles();
-        if(listFiles!=null){
-            for(File curr:listFiles){
-                if(!curr.isHidden() && curr.isDirectory()){
-                    ans.addAll(getSongs(curr));
-                }
-                else{
-                    if(curr.getName().endsWith(".mp3") && !curr.getName().startsWith(".")){
-                        ans.add(curr);
-                    }
-                }
-            }
-        }
-        return ans;
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        Intent backToMain = getIntent();
+//        int song_position = backToMain.getIntExtra("com.example.musicplayer.PlaySong.SongPosition", 0);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
 
 
--- app/src/main/java/com/example/musicplayer/PlaySong.java
+++ app/src/main/java/com/example/musicplayer/PlaySong.java
@@ -1,8 +1,10 @@
 package com.example.musicplayer;
 
 import static com.example.musicplayer.MainActivity.getSongs;
+import static com.example.musicplayer.MainActivity.Songs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
@@ -27,26 +29,27 @@ public class PlaySong extends AppCompatActivity {
     private ImageButton next;
     private SeekBar seekBar;
     private MediaPlayer mediaPlayer;
-    private ArrayList<File> Songs;
+    //private ArrayList<File> Songs;
     private static boolean isPlaying = false;
     private Thread UpdateSeek;
+    int[] song_position = new int[0];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_song);
         Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
-        Songs = (ArrayList) bundle.getParcelableArrayList("com.example.musicplayer.CustomAdapter.SongList");
+        //Bundle bundle = intent.getExtras();
+        //Songs = (ArrayList) bundle.getParcelableArrayList("com.example.musicplayer.CustomAdapter.SongList");
         String song_name = intent.getStringExtra("com.example.musicplayer.CustomAdapter.SongName");
-        final int[] song_position = {intent.getIntExtra("com.example.musicplayer.CustomAdapter.SongPosition", 0)};
+        song_position = new int[]{intent.getIntExtra("com.example.musicplayer.CustomAdapter.SongPosition", 0)};
         textView = findViewById(R.id.mp_song_text);
         previous = findViewById(R.id.previous);
         play_pause = findViewById(R.id.play_pause);
         next = findViewById(R.id.next);
         seekBar = findViewById(R.id.seekBar);
 
-
+       // File[] ext = ContextCompat.getExternalFilesDirs(PlaySong.this, )
         //Added for Marquee
         textView.setSelected(true);
         textView.setText(song_name);
@@ -66,7 +69,12 @@ public class PlaySong extends AppCompatActivity {
                 Uri uri = Uri.parse(Songs.get(song_position[0]).toString());
                 textView.setText(Songs.get(song_position[0]).getName());
                 mediaPlayer = MediaPlayer.create(PlaySong.this, uri);
-                mediaPlayer.start();
+                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        mediaPlayer.start();
+                    }
+                });
                 isPlaying = true;
                 seekBar.setProgress(0);
                 play_pause.setImageResource(R.drawable.ic_baseline_pause_24);
@@ -103,10 +111,18 @@ public class PlaySong extends AppCompatActivity {
                 Uri uri = Uri.parse(Songs.get(song_position[0]).toString());
                 textView.setText(Songs.get(song_position[0]).getName().replace(".mp3", ""));
                 mediaPlayer = MediaPlayer.create(PlaySong.this, uri);
-                mediaPlayer.start();
+                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        mediaPlayer.start();
+                    }
+                });
                 isPlaying = true;
                 seekBar.setProgress(0);
                 play_pause.setImageResource(R.drawable.ic_baseline_pause_24);
+//                synchronized (UpdateSeek){
+//                    UpdateSeek.notify();
+//                }
             }
         });
 
@@ -117,9 +133,11 @@ public class PlaySong extends AppCompatActivity {
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 if(b && isPlaying){
                     mediaPlayer.seekTo(i);
+                    //seekBar.setProgress(i);
                 }
                 else if(b && !mediaPlayer.isPlaying()) {
                     mediaPlayer.seekTo(i);
+                    //seekBar.setProgress(i);
                     mediaPlayer.pause();
                     isPlaying = false;
                 }
@@ -150,7 +168,12 @@ public class PlaySong extends AppCompatActivity {
         //Starts Playing as soon as a song is clicked
         Uri uri = Uri.parse(Songs.get(song_position[0]).toString());
         mediaPlayer = MediaPlayer.create(this, uri);
-        mediaPlayer.start();
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
         isPlaying =true;
         seekBar.setMax(mediaPlayer.getDuration());
 
@@ -159,16 +182,45 @@ public class PlaySong extends AppCompatActivity {
             @Override
             public void run() {
                 int CurrentPosition = 0;
-                while (CurrentPosition < mediaPlayer.getDuration()) {
-                    CurrentPosition = mediaPlayer.getCurrentPosition();
-                    seekBar.setProgress(CurrentPosition);
+                try{
+                while (mediaPlayer!=null && CurrentPosition < mediaPlayer.getDuration()) {
                     try {
+                        if (mediaPlayer != null & mediaPlayer.isPlaying())
+                            CurrentPosition = mediaPlayer.getCurrentPosition();
+                        if (seekBar != null)
+                            seekBar.setProgress(CurrentPosition);
                         Thread.sleep(1000);
-                    } catch (InterruptedException e) {
+                    }
+                    catch (NullPointerException e){
+                        e.printStackTrace();
+                    }
+                    catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    catch (IllegalStateException e) {
+                        e.printStackTrace();
+                    }
+                    catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
-                //Thread.currentThread().interrupt();
+
+//                    try {
+//                        synchronized (this) {
+//                            this.wait();
+//                        }
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+
+                }
+                catch(IllegalStateException e){
+                    e.printStackTrace();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
             }
         };
 
@@ -176,15 +228,37 @@ public class PlaySong extends AppCompatActivity {
 
 
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        Intent backToMain = new Intent(PlaySong.this, MainActivity.class);
+//        backToMain.putExtra("com.example.musicplayer.PlaySong.SongPosition", song_position[0]);
+//        startActivity(backToMain);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        Intent backToMain = getIntent();
+//        int song_position = backToMain.getIntExtra("com.example.musicplayer.PlaySong.SongPosition", 0);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mediaPlayer.stop();
-        mediaPlayer.release();
         isPlaying = false;
         UpdateSeek.interrupt();
         UpdateSeek = null;
+        mediaPlayer.stop();
+        mediaPlayer.reset();
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
+
 }
